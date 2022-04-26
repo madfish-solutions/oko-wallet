084bc0615432d074f3bda75f6645d75e7591e8c1
Andrii Ivaniv <unixvb@gmail.com>
2022-04-26 18:52:41 +0300

    ts fix

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,28 +1,31 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-  watchFolders: [
-    path.resolve(__dirname, '../../libs/ui'),
-  ],
-  resolver: {
-    extraNodeModules: new Proxy(
-      {},
-      {
-        get: (target, name) => {
-          if (target.hasOwnProperty(name)) {
-            return target[name]
-          }
-          return path.join(process.cwd(), `node_modules/${name}`)
-        },
-      },
-    ),
-  }
+    transformer: {
+        getTransformOptions: async () => ({
+            transform: {
+                experimentalImportSupport: false,
+                inlineRequires: true,
+            },
+        }),
+    },
+    watchFolders: [
+        path.resolve(__dirname, '../../libs/ui'),
+    ],
+    resolver: {
+        disableHierarchicalLookup: true,
+        nodeModulesPaths: [path.resolve(__dirname, './node_modules')],
+        extraNodeModules: new Proxy(
+            {},
+            {
+                get: (target, name) => {
+                    if (target.hasOwnProperty(name)) {
+                        return target[name]
+                    }
+
+                    return path.join(process.cwd(), `node_modules/${name}`)
+                }
+            }
+        )
+    }
 };
