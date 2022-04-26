368b142aeb596a5240e42f1c547d2ae5cc8179b3
Andrii Ivaniv <unixvb@gmail.com>
2022-04-27 00:23:08 +0300

    jest setup

---
+++ libs/ui/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  preset: 'react-native',
+  setupFiles: ['<rootDir>/jest.setup.js'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?@react-native|react-native|react-native-themis|@react-navigation/.*))'
+  ],
+  moduleNameMapper: {
+    '\\.svg': '<rootDir>/src/mocks/svg.mock.js',
+    '^react-native-themis': '<rootDir>/node_modules/react-native-themis/src/index.js'
+  },
+  timers: 'legacy'
+};
