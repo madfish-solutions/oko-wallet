1ac637e3717bea02a67319fdd0c83c139cddc60c
svyatoslavtt <stiven22235@gmail.com>
2022-04-27 20:02:50 +0300

    Add '@rnw-community/shared' package

--- libs/ui/src/utils/is-string.util.ts
+++
@@ -1,6 +0,0 @@
-import { isDefined } from './is-defined';
-
-type MaybeString = string | number | boolean | object | undefined | null;
-
-export const isString = (str: MaybeString): str is string =>
-  isDefined(str) && typeof str === 'string' && str.length !== 0;
