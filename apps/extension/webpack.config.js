372d035325a4f2451fc8960e8ff202704522b30a
svyatoslavtt <stiven22235@gmail.com>
2022-04-28 18:43:26 +0300

    Add config to webpack

--- apps/extension/webpack.config.js
+++ apps/extension/webpack.config.js
@@ -1,6 +1,7 @@
 const CopyPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const webpack = require('webpack');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 const appDirectory = path.resolve(__dirname);
@@ -59,6 +60,7 @@ module.exports = {
   },
 
   resolve: {
+    mainFields: ['browser', 'main', 'module'],
     alias: {
       'react-native$': 'react-native-web'
     },
@@ -82,6 +84,9 @@ module.exports = {
   },
 
   plugins: [
+    new webpack.DefinePlugin({
+      __DEV__: JSON.stringify(process.env.NODE_ENV === 'development')
+    }),
     new CopyPlugin({
       patterns: [
         {
