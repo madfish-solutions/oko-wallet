8a2bcd3ad0653c2eb5026a550457812e7ff5e245
svyatoslavtt <stiven22235@gmail.com>
2022-04-29 16:46:13 +0300

    Add async-storage test

--- libs/ui/src/app/app.tsx
+++ libs/ui/src/app/app.tsx
@@ -1,7 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React, { useEffect } from 'react';
-import { Platform } from 'react-native';
+import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
@@ -13,23 +12,16 @@ import { persistor, store } from '../store/store';
 
 const Stack = createNativeStackNavigator<ScreensParamList>();
 
-export const App = () => {
-  useEffect(() => {
-    console.log('Shared App component rendering');
-    console.log('OS:', Platform.OS);
-  }, []);
-
-  return (
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen name={ScreensEnum.Welcome} component={WelcomeScreen} />
-            <Stack.Screen name={ScreensEnum.Test} component={TestScreen} />
-            <Stack.Screen name={ScreensEnum.Inner} component={InnerScreen} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </PersistGate>
-    </Provider>
-  );
-};
+export const App = () => (
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name={ScreensEnum.Welcome} component={WelcomeScreen} />
+          <Stack.Screen name={ScreensEnum.Test} component={TestScreen} />
+          <Stack.Screen name={ScreensEnum.Inner} component={InnerScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PersistGate>
+  </Provider>
+);
