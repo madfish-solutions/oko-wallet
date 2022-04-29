a2b1b8efed34469c0e4a0ca4858ace403ceb405d
svyatoslavtt <stiven22235@gmail.com>
2022-04-29 17:01:31 +0300

    Add async to render test, return log to App

--- libs/ui/src/app/app.tsx
+++ libs/ui/src/app/app.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Platform } from 'react-native';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import { Navigator } from '../components/navigator/navigator';
 import { persistor, store } from '../store/store';
 
-export const App = () => (
-  <Provider store={store}>
-    <PersistGate persistor={persistor}>
-      <Navigator />
-    </PersistGate>
-  </Provider>
-);
+export const App = () => {
+  useEffect(() => {
+    console.log('Shared App component rendering');
+    console.log('OS:', Platform.OS);
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Navigator />
+      </PersistGate>
+    </Provider>
+  );
+};
