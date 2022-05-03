d1fe46071f63330b52a51924291677401482ecd6
svyatoslavtt <stiven22235@gmail.com>
2022-05-03 15:14:24 +0300

    Refactor selector

--- libs/ui/src/mocks/account.interface.mock.ts
+++ libs/ui/src/mocks/account.interface.mock.ts
@@ -4,7 +4,14 @@ import { AccountInterface } from '../store/interfaces/account.interface';
 export const mockHdAccount: AccountInterface = {
   name: 'Mock HD Account',
   type: AccountTypeEnum.HD_ACCOUNT,
-  publicKey: '',
+  publicKey: 'publicKey',
   // Klaytn test account pkh
   publicKeyHash: '0x84757a438E06631f34b2199B5D92e6865cE47D50'
+};
+
+export const initialAccount: AccountInterface = {
+  name: '',
+  type: AccountTypeEnum.HD_ACCOUNT,
+  publicKey: '',
+  publicKeyHash: ''
 };
