bd999356e7678258103a2935d4b000efcd08d3b5
razbeikov <razbeikov.oleksandr@gmail.com>
2022-05-04 10:07:37 +0300

    fix: delete console log and rename

--- libs/ui/src/utils/shelter.util.ts
+++ libs/ui/src/utils/shelter.util.ts
@@ -1,6 +1,6 @@
 import { Buffer } from 'buffer';
 
-import { generateSalt } from '../themis/encrypt';
+import { generateSalt } from '../themis/generate-salt';
 
 export const getStoredValue = (key: string) => {
   try {
@@ -27,8 +27,6 @@ export const encrypt = async (seed: string, passwordHash: string) => {
 
   const secureText = new Uint8Array([...Buffer.from(seed)]);
   const encrypted = salt.encrypt(secureText);
-  console.log(encrypted.length);
-  console.log(secureText.length);
 
   return encrypted;
 };
@@ -36,12 +34,7 @@ export const encrypt = async (seed: string, passwordHash: string) => {
 export const decrypt = async (passwordHash: string, key: string) => {
   const seedPhrase: Uint8Array = await getStoredValue(key);
   const salt = await generateSalt(passwordHash);
-  console.log(seedPhrase);
-  console.log(typeof seedPhrase);
-  console.log(Object.keys(seedPhrase));
-  console.log(Object.values(seedPhrase));
   const seedArray = Uint8Array.from(Object.values(seedPhrase));
-  console.log(seedArray);
   const decryptedArray = salt.decrypt(seedArray);
   const decrypted = Buffer.from(decryptedArray).toString();
 
