9037c7693dcf97039338ad42d789bd71cff12b87
svyatoslavtt <stiven22235@gmail.com>
2022-05-05 11:17:17 +0300

    Add get balance for tezos

--- libs/ui/src/utils/get-gas-token-balance.ts
+++ libs/ui/src/utils/get-gas-token-balance.ts
@@ -2,18 +2,37 @@ import { getDefaultProvider } from 'ethers';
 import { from, map } from 'rxjs';
 
 import { NETWORKS } from '../constants/networks';
+import { NetworksNameEnum } from '../enums/network.enum';
 
 import { convertUnits } from './convertUnits';
+import { readOnlySignerAccount } from './read-only.signer.util';
+import { createReadOnlyTezosToolkit } from './rpc/tezos-toolkit.utils';
 
 export const getGasTokenBalance$ = (network: string, pkh: string) => {
   const { gasToken, rpc } = NETWORKS[network];
+
+  const getConvertedBalancePure = (balance: number): string =>
+    convertUnits(balance, gasToken.decimals)
+      .decimalPlaces(gasToken.decimals ?? 6)
+      .toFixed();
+
+  if (network === NetworksNameEnum.Tezos) {
+    // TODO: Delete later
+    const tzAddress = 'tz1XstX8fYXPY5JNV6M2p1yLD6VNjX38YuQP';
+
+    return from(createReadOnlyTezosToolkit(rpc, readOnlySignerAccount).tz.getBalance(tzAddress)).pipe(
+      map(balance => ({
+        gasTokenBalance: getConvertedBalancePure(+balance),
+        gasToken
+      }))
+    );
+  }
+
   const provider = getDefaultProvider(rpc);
 
   return from(provider.getBalance(pkh)).pipe(
     map(balance => ({
-      gasTokenBalance: convertUnits(+balance, gasToken.decimals)
-        .decimalPlaces(gasToken.decimals ?? 6)
-        .toFixed(),
+      gasTokenBalance: getConvertedBalancePure(+balance),
       gasToken
     }))
   );
---
+++ libs/ui/src/utils/read-only.signer.util.ts
@@ -0,0 +1,35 @@
+import { Signer } from '@taquito/taquito';
+
+import { AccountTypeEnum } from '../enums/account-type.enum';
+import { AccountInterface } from '../store/interfaces/account.interface';
+
+import { getEnv } from './env.utils';
+
+export class ReadOnlySigner implements Signer {
+  constructor(private pkh: string, private pk: string) {}
+
+  async publicKeyHash() {
+    return this.pkh;
+  }
+  async publicKey() {
+    return this.pk;
+  }
+  async secretKey(): Promise<string> {
+    throw new Error('Secret key cannot be exposed');
+  }
+  async sign(): Promise<{
+    bytes: string;
+    sig: string;
+    prefixSig: string;
+    sbytes: string;
+  }> {
+    throw new Error('Cannot sign');
+  }
+}
+
+export const readOnlySignerAccount: AccountInterface = {
+  name: 'Read-only account',
+  type: AccountTypeEnum.IMPORTED_ACCOUNT,
+  publicKey: getEnv('READ_ONLY_SIGNER_PUBLIC_KEY'),
+  publicKeyHash: getEnv('READ_ONLY_SIGNER_PUBLIC_KEY_HASH')
+};
