9aa0cf008a604b95a21613487f9dcf30d6a31c30
razbeikov <razbeikov.oleksandr@gmail.com>
2022-05-06 14:25:36 +0300

    fix async error

--- libs/ui/src/shelter/shelter.ts
+++ libs/ui/src/shelter/shelter.ts
@@ -6,10 +6,13 @@ import { setStoredValue } from '../utils/store.util';
 export class Shelter {
   static saveSensitiveData = async (sensetiveData: Record<string, string>, password: string) => {
     const passwordHash = await hashPassword(password);
-    Object.entries(sensetiveData).map(async ([key, value]) => {
-      const dataToSave = await encrypt(value, passwordHash);
-      setStoredValue(key, JSON.stringify(dataToSave));
-    });
+
+    return Promise.all(
+      Object.entries(sensetiveData).map(async ([key, value]) => {
+        const dataToSave = await encrypt(value, passwordHash);
+        setStoredValue(key, JSON.stringify(dataToSave));
+      })
+    );
   };
 
   static decryptSensitiveData = async (key: string, password: string) => {
