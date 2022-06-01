6277717f48cc4da69f3b72830aaa0156d19ed6f1
razbeikov <razbeikov.oleksandr@gmail.com>
2022-06-01 12:16:50 +0300

    add search input in token list

--- libs/ui/src/screens/wallet/wallet.tsx
+++ libs/ui/src/screens/wallet/wallet.tsx
@@ -1,8 +1,9 @@
-import React, { FC } from 'react';
-import { View, Text } from 'react-native';
+import React, { FC, useEffect, useState } from 'react';
+import { View, Text, TextInput } from 'react-native';
 
 import { NavigationBar } from '../../components/navigation-bar/navigation-bar';
 import { Networks } from '../../components/networks/networks';
+import { Token } from '../../interfaces/token.interface';
 import { useVisibleAccountTokensSelector, useSelectedNetworkSelector } from '../../store/wallet/wallet.selectors';
 
 import { AccountTokens } from './components/account-tokens/account-tokens';
@@ -11,6 +12,18 @@ import { WalletStyles } from './wallet.styles';
 export const Wallet: FC = () => {
   const { gasTokenMetadata, gasTokenBalance } = useSelectedNetworkSelector();
   const visibleAccountTokens = useVisibleAccountTokensSelector();
+  const [inputNameSearch, setInputNameSearch] = useState('');
+  const [FiltredTokens, setFiltredTokens] = useState<Token[]>([]);
+
+  useEffect(() => {
+    setFiltredTokens(
+      visibleAccountTokens.filter(
+        ({ name, symbol }) =>
+          name.toLowerCase().includes(inputNameSearch.toLowerCase()) ||
+          symbol.toLowerCase().includes(inputNameSearch.toLowerCase())
+      )
+    );
+  }, [inputNameSearch]);
 
   const gasTokenBalanceWithLoading = gasTokenBalance.isLoading
     ? '...'
@@ -24,7 +37,13 @@ export const Wallet: FC = () => {
           Balance: <Text style={WalletStyles.boldText}>{gasTokenBalanceWithLoading}</Text>
         </Text>
         <Networks />
-        <AccountTokens visibleAccountTokens={visibleAccountTokens} />
+        <TextInput
+          style={WalletStyles.input}
+          onChangeText={setInputNameSearch}
+          value={inputNameSearch}
+          placeholder="find token..."
+        />
+        <AccountTokens visibleAccountTokens={inputNameSearch ? FiltredTokens : visibleAccountTokens} />
       </View>
     </View>
   );
