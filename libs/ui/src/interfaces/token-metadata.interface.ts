adfb57d152dccccfb46e52ed205941ae053ed5f1
Aleks Razbeikov <58780063+Boltman92@users.noreply.github.com>
2022-06-03 16:40:47 +0300

    add collectibles screen (#26)
    * add collectibles screen
    
    * delete add collectibles screen and extend add token screen
    
    * add missed input
    
    * fix typo

--- libs/ui/src/interfaces/token-metadata.interface.ts
+++ libs/ui/src/interfaces/token-metadata.interface.ts
@@ -1,7 +1,9 @@
 export interface TokenMetadata {
   tokenAddress: string;
+  tokenId?: string;
   name: string;
   decimals: number;
   symbol: string;
   thumbnailUri?: string;
+  artifactUri?: string;
 }
--- libs/ui/src/screens/add-new-token/add-new-token.tsx
+++ libs/ui/src/screens/add-new-token/add-new-token.tsx
@@ -12,27 +12,43 @@ export const AddNewToken: FC = () => {
   const [symbol, setSymbol] = useState('');
   const [decimals, setDecimals] = useState('');
   const [thumbnailUri, seThumbnailUri] = useState('');
+  const [artifactUri, setArtifactUri] = useState('');
+  const [tokenId, setTokenId] = useState('');
 
   const onDecimalsChange = (decimalsValue: string) => setDecimals(decimalsValue.replace(/\D/g, ''));
 
   const onAddToken = () => {
-    dispatch(addTokenMetadataAction({ tokenAddress, name, symbol, thumbnailUri, decimals: Number(decimals) }));
+    dispatch(
+      addTokenMetadataAction({
+        tokenAddress,
+        tokenId,
+        name,
+        symbol,
+        thumbnailUri,
+        artifactUri,
+        decimals: Number(decimals)
+      })
+    );
 
     setTokenAddress('');
     setName('');
     setSymbol('');
     setDecimals('');
     seThumbnailUri('');
+    setArtifactUri('');
+    setTokenId('');
   };
 
   return (
     <View>
       <NavigationBar />
       <TextInput placeholder="Token Address" value={tokenAddress} onChangeText={setTokenAddress} />
+      <TextInput placeholder="Token ID" value={tokenId} onChangeText={setTokenId} />
       <TextInput placeholder="Token Name" value={name} onChangeText={setName} />
       <TextInput placeholder="Token Symbol" value={symbol} onChangeText={setSymbol} />
       <TextInput placeholder="Token Decimals" value={decimals} onChangeText={onDecimalsChange} keyboardType="numeric" />
       <TextInput placeholder="Thumbnail Uri" value={thumbnailUri} onChangeText={seThumbnailUri} />
+      <TextInput placeholder="artifact Uri" value={artifactUri} onChangeText={setArtifactUri} />
       <Pressable onPress={onAddToken}>
         <Text>Add token</Text>
       </Pressable>
