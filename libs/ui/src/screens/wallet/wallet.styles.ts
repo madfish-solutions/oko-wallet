fe53fe5114ee8126db5fb24d769093d70f67d400
razbeikov <razbeikov.oleksandr@gmail.com>
2022-06-08 13:02:21 +0300

    refactoring

--- libs/ui/src/screens/wallet/wallet.styles.ts
+++ libs/ui/src/screens/wallet/wallet.styles.ts
@@ -1,7 +1,8 @@
-import { StyleSheet, Platform } from 'react-native';
+import { isWeb } from '@rnw-community/platform';
+import { StyleSheet } from 'react-native';
 
 export const WalletStyles = StyleSheet.create({
   root: {
-    height: Platform.OS === 'web' ? '100vh' : '100%'
+    height: isWeb ? '100vh' : '100%'
   }
 });
