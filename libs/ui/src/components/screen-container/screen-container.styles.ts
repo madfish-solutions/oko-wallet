2f912d62a7d839e6a264789be930ee53410c11dd
razbeikov <razbeikov.oleksandr@gmail.com>
2022-06-08 17:44:13 +0300

    refactored using getCustomSize func

--- libs/ui/src/components/screen-container/screen-container.styles.ts
+++ libs/ui/src/components/screen-container/screen-container.styles.ts
@@ -1,13 +1,14 @@
 import { StyleSheet } from 'react-native';
 
 import { colors } from '../../styles/colors';
+import { getCustomSize } from '../../styles/format-size';
 
 export const ScreenContainerStyles = StyleSheet.create({
   root: {
     flexGrow: 1,
     flexShrink: 1,
     backgroundColor: colors.bgGrey1,
-    paddingLeft: '24px',
-    paddingRight: '24px'
+    paddingLeft: getCustomSize(3),
+    paddingRight: getCustomSize(3)
   }
 });
