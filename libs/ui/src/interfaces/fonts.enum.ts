6875f8591acd851110cb88807193ded4f7fe2f0c
Svyatoslav Tyutyunnik <stiven22235@gmail.com>
2022-06-13 17:35:23 +0300

    Add fonts (#34)
    * Add fonts
    
    * Add fonts
    
    * Add correct font-family name
    
    * Remove useless code
    
    * Remove unused files
    
    * Remove useless code
    
    * Add type to fonts

---
+++ libs/ui/src/interfaces/fonts.enum.ts
@@ -0,0 +1,7 @@
+export enum FontsEnum {
+  interRegular = 'Inter-Regular',
+  interBold = 'Inter-Bold',
+  interSemiBold = 'Inter-SemiBold',
+  ibmPlexSansBold = 'IBMPlexSans-Bold',
+  ibmPlexSansMedium = 'IBMPlexSans-Medium'
+}
