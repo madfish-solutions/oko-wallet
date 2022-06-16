75089538481140481460a180a4fe39f24a0792f4
svyatoslavtt <stiven22235@gmail.com>
2022-06-16 12:14:07 +0300

    Remove unsued code

--- libs/ui/src/screens/wallet/wallet.tsx
+++ libs/ui/src/screens/wallet/wallet.tsx
@@ -1,25 +1,17 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC } from 'react';
 import { ScrollView, View } from 'react-native';
-import { useDispatch } from 'react-redux';
 
 import { Account } from '../../components/account/account';
 import { GasTokenBalance } from '../../components/gas-token-balance/gas-token-balance';
 import { Header } from '../../components/header/header';
 import { NavigationBar } from '../../components/navigation-bar/navigation-bar';
 import { useUnlock } from '../../hooks/use-unlock.hook';
-import { resetApplicationAction } from '../../store/root-state.actions';
 
 import { AccountTokens } from './components/account-tokens/account-tokens';
 import { WalletStyles } from './wallet.styles';
 
 export const Wallet: FC = () => {
   const { isLocked } = useUnlock();
-  const dispatch = useDispatch();
-
-  // useEffect(() => {
-  //   console.log('here');
-  //   dispatch(resetApplicationAction.submit());
-  // }, []);
 
   return (
     <View style={WalletStyles.root}>
