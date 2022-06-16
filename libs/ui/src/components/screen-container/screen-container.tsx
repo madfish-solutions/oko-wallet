dabcee41a0f73b098e44cbd7601f3ac1cb49bc7f
svyatoslavtt <stiven22235@gmail.com>
2022-06-16 18:52:47 +0300

    Add imptoves to screen-container

--- libs/ui/src/components/screen-container/screen-container.tsx
+++ libs/ui/src/components/screen-container/screen-container.tsx
@@ -3,11 +3,11 @@ import { StyleProp, ViewStyle, ScrollView, ScrollViewProps } from 'react-native'
 
 import { ScreenContainerStyles } from './screen-container.styles';
 
-type Props = {
+interface Props extends ScrollViewProps {
   style?: StyleProp<ViewStyle>;
-} & ScrollViewProps;
+}
 
-export const ScreenContainer: FC<Props> = ({ children, style, ...props }) => (
+export const ScreenContainer: FC<Props> = ({ style, children, ...props }) => (
   <ScrollView {...props} style={[ScreenContainerStyles.root, style]}>
     {children}
   </ScrollView>
