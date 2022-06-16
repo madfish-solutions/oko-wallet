7141dd8192fdc2e0ca9264de5d9d7c2839054977
Mykola Medvedenko <35599200+MuKoJla@users.noreply.github.com>
2022-06-16 17:07:59 +0300

    Add Button component (#39)
    * add Button component
    
    * fixes
    
    * add font
    
    * fixes
    
    * fixes

---
+++ libs/ui/src/components/button/button.tsx
@@ -0,0 +1,40 @@
+import React, { FC } from 'react';
+import { Pressable, PressableProps, Text, View } from 'react-native';
+
+import { getCustomSize } from '../../styles/format-size';
+import { Icon } from '../icon/icon';
+import { IconNameEnum } from '../icon/icon-name.enum';
+
+import { styles as buttonStyles } from './button.styles';
+import { Theme } from './types';
+
+interface Props extends PressableProps {
+  title: string;
+  theme?: Theme;
+  rightIcon?: IconNameEnum;
+  leftIcon?: IconNameEnum;
+  iconSize?: number;
+}
+
+const defaultIconSize = getCustomSize(2);
+
+export const Button: FC<Props> = ({
+  title,
+  theme = 'primary',
+  rightIcon,
+  leftIcon,
+  iconSize = defaultIconSize,
+  ...restProps
+}) => {
+  const styles = buttonStyles(theme);
+
+  return (
+    <Pressable {...restProps} style={styles.root}>
+      <View style={styles.wrapper}>
+        {leftIcon && <Icon name={leftIcon} size={iconSize} style={styles.leftIcon} />}
+        <Text style={styles.text}>{title}</Text>
+        {rightIcon && <Icon name={rightIcon} size={iconSize} style={styles.rightIcon} />}
+      </View>
+    </Pressable>
+  );
+};
---
+++ libs/ui/src/components/button/constants/themes.ts
@@ -0,0 +1,12 @@
+import { colors } from '../../../styles/colors';
+
+export const themes = {
+  primary: {
+    color: colors.textGrey1,
+    backgroundColor: colors.navGrey1
+  },
+  secondary: {
+    color: colors.textGrey1,
+    backgroundColor: colors.orange
+  }
+};
