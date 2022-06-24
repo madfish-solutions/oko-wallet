d2b6599687720d0d7aebaaf854b41f70e6b9da38
Andrii Ivaniv <unixvb@users.noreply.github.com>
2022-06-24 17:26:48 +0300

    extension build flow added (#55)
    * extension build flow added
    
    * extension build flow added
    
    * appcenter ms setup

--- apps/mobile/ios/KlaytnWallet/AppDelegate.mm
+++ apps/mobile/ios/KlaytnWallet/AppDelegate.mm
@@ -6,6 +6,10 @@
 
 #import <React/RCTAppSetupUtils.h>
 
+#import <AppCenterReactNative.h>
+#import <AppCenterReactNativeAnalytics.h>
+#import <AppCenterReactNativeCrashes.h>
+
 #if RCT_NEW_ARCH_ENABLED
 #import <React/CoreModulesPlugins.h>
 #import <React/RCTCxxBridgeDelegate.h>
