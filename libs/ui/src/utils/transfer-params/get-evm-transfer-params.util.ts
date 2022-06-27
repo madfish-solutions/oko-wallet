c99c5945fbfb5d62470ef1c046d9cd0a5d5e995b
svyatoslavtt <stiven22235@gmail.com>
2022-06-27 12:31:52 +0300

    Add evm transfer params right flow

--- libs/ui/src/utils/transfer-params/get-evm-transfer-params.util.ts
+++ libs/ui/src/utils/transfer-params/get-evm-transfer-params.util.ts
@@ -1,11 +1,11 @@
 import { TransactionRequest as EvmTransferParams } from '@ethersproject/abstract-provider';
-import { BigNumber } from 'bignumber.js';
 import { ethers } from 'ethers';
-import { map, Observable, of } from 'rxjs';
+import { map, Observable, switchMap } from 'rxjs';
 
 import { AccountInterface } from '../../interfaces/account.interface';
 import { NetworkInterface } from '../../interfaces/network.interface';
 import { SendAssetPayload } from '../../interfaces/send-asset-action-payload.interface';
+import { Shelter } from '../../shelter/shelter';
 import { getDefaultEvmProvider } from '../get-default-evm-provider.utils';
 import { getString } from '../get-string.utils';
 
@@ -19,12 +19,19 @@ export const getEvmTransferParams$ = (
   const { tokenAddress } = asset;
   const { rpcUrl, networkType } = selectedNetwork;
   const senderPublicKeyHash = getString(sender.networksKeys[networkType]?.publicKeyHash);
-  const amountBN = new BigNumber(amount);
+  const amountBN = ethers.utils.parseUnits(amount, 18);
 
   const provider = getDefaultEvmProvider(rpcUrl);
   const tokenContract = new ethers.Contract(tokenAddress, ERC20ABI, provider);
 
-  return of(tokenContract).pipe(
-    map(contract => contract.connect(senderPublicKeyHash).transfer(receiverPublicKeyHash, amountBN))
+  return Shelter.getEvmSigner$(senderPublicKeyHash, provider).pipe(
+    map(signer => tokenContract.connect(signer)),
+    switchMap(tokenSigner => tokenSigner.transfer(receiverPublicKeyHash, amountBN) as Observable<EvmTransferParams>),
+    map(params => ({
+      ...params,
+      gasLimit: params.gasLimit?.toString(),
+      gasPrice: params.gasPrice?.toString(),
+      value: params.value?.toString()
+    }))
   );
 };
