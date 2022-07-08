0ce4ab454199b431f9b68f1aaaf6e00fff154135
svyatoslavtt <stiven22235@gmail.com>
2022-07-08 17:46:15 +0300

    Separate search from list

--- libs/ui/src/modals/components/modal-flat-list/modal-flat-list.tsx
+++ libs/ui/src/modals/components/modal-flat-list/modal-flat-list.tsx
@@ -1,19 +1,12 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { useForm, Controller } from 'react-hook-form';
+import React from 'react';
 import { View, FlatList, FlatListProps } from 'react-native';
 
-import { IconNameEnum } from '../../../components/icon/icon-name.enum';
-import { Row } from '../../../components/row/row';
-import { TextInput } from '../../../components/text-input/text-input';
-import { TouchableIcon } from '../../../components/touchable-icon/touchable-icon';
-import { EMPTY_STRING } from '../../../constants/defaults';
 import { getItemLayout } from '../../utils/get-item-layout.util';
+import { ModalSearch } from '../modal-search/modal-search';
 
 import { styles } from './modal-flat-list.styles';
 
-const SEARCH_FIELD = 'search';
-
-interface Props<T> extends Pick<FlatListProps<T>, 'renderItem' | 'data' | 'keyExtractor'> {
+interface Props<T extends { name: string }> extends Pick<FlatListProps<T>, 'renderItem' | 'data' | 'keyExtractor'> {
   onPressAddIcon: () => void;
   flatListRef: React.RefObject<FlatList<T>>;
   searchValue?: string;
@@ -21,7 +14,7 @@ interface Props<T> extends Pick<FlatListProps<T>, 'renderItem' | 'data' | 'keyEx
   selectedItem: T;
 }
 
-export const ModalFlatList = <T extends unknown>({
+export const ModalFlatList = <T extends { name: string }>({
   onPressAddIcon,
   flatListRef,
   data,
@@ -29,67 +22,17 @@ export const ModalFlatList = <T extends unknown>({
   setSearchValue,
   keyExtractor,
   selectedItem
-}: Props<T>) => {
-  const [isShowSearchField, setIsShowSearchField] = useState(false);
+}: Props<T>) => (
+  <View style={styles.root}>
+    <ModalSearch onPressAddIcon={onPressAddIcon} setSearchValue={setSearchValue} selectedItem={selectedItem.name} />
 
-  const { control, resetField, watch, setFocus } = useForm({
-    mode: 'onChange',
-    defaultValues: {
-      search: EMPTY_STRING
-    }
-  });
-
-  const searchValue = watch(SEARCH_FIELD);
-
-  useEffect(() => {
-    setSearchValue(searchValue);
-  }, [searchValue, setSearchValue]);
-
-  useEffect(() => {
-    if (isShowSearchField) {
-      setFocus(SEARCH_FIELD);
-    }
-  }, [isShowSearchField]);
-
-  const closeSearchField = useCallback(() => {
-    setIsShowSearchField(false);
-    resetField(SEARCH_FIELD);
-  }, []);
-
-  useEffect(() => {
-    closeSearchField();
-  }, [selectedItem, closeSearchField]);
-
-  return (
-    <View style={styles.root}>
-      <Row style={styles.search}>
-        {isShowSearchField ? (
-          <>
-            <Controller
-              control={control}
-              name={SEARCH_FIELD}
-              render={({ field: { onChange, value, ref } }) => (
-                <TextInput ref={ref} value={value} onChangeText={onChange} placeholder="Search" />
-              )}
-            />
-            <TouchableIcon name={IconNameEnum.X} onPress={closeSearchField} style={styles.close} />
-          </>
-        ) : (
-          <>
-            <TouchableIcon name={IconNameEnum.Search} onPress={() => setIsShowSearchField(true)} />
-            <TouchableIcon name={IconNameEnum.Add} onPress={onPressAddIcon} />
-          </>
-        )}
-      </Row>
-
-      <FlatList
-        ref={flatListRef}
-        getItemLayout={getItemLayout}
-        data={data}
-        showsVerticalScrollIndicator={false}
-        renderItem={renderItem}
-        keyExtractor={keyExtractor}
-      />
-    </View>
-  );
-};
+    <FlatList
+      ref={flatListRef}
+      getItemLayout={getItemLayout}
+      data={data}
+      showsVerticalScrollIndicator={false}
+      renderItem={renderItem}
+      keyExtractor={keyExtractor}
+    />
+  </View>
+);
---
+++ libs/ui/src/modals/components/modal-search/modal-search.tsx
@@ -0,0 +1,71 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import { useForm, Controller } from 'react-hook-form';
+
+import { IconNameEnum } from '../../../components/icon/icon-name.enum';
+import { Row } from '../../../components/row/row';
+import { TextInput } from '../../../components/text-input/text-input';
+import { TouchableIcon } from '../../../components/touchable-icon/touchable-icon';
+import { EMPTY_STRING } from '../../../constants/defaults';
+import { styles } from '../modal-flat-list/modal-flat-list.styles';
+
+const SEARCH_FIELD = 'search';
+
+interface Props {
+  setSearchValue: (text: string) => void;
+  onPressAddIcon: () => void;
+  selectedItem: string;
+}
+
+export const ModalSearch: React.FC<Props> = ({ setSearchValue, onPressAddIcon, selectedItem }) => {
+  const [isShowSearchField, setIsShowSearchField] = useState(false);
+
+  const { control, resetField, watch, setFocus } = useForm({
+    mode: 'onChange',
+    defaultValues: {
+      search: EMPTY_STRING
+    }
+  });
+
+  const searchValue = watch(SEARCH_FIELD);
+
+  useEffect(() => {
+    setSearchValue(searchValue);
+  }, [searchValue, setSearchValue]);
+
+  useEffect(() => {
+    if (isShowSearchField) {
+      setFocus(SEARCH_FIELD);
+    }
+  }, [isShowSearchField]);
+
+  const closeSearchField = useCallback(() => {
+    setIsShowSearchField(false);
+    resetField(SEARCH_FIELD);
+  }, []);
+
+  useEffect(() => {
+    closeSearchField();
+  }, [selectedItem, closeSearchField]);
+
+  return (
+    <Row style={styles.search}>
+      {isShowSearchField ? (
+        <>
+          <Controller
+            control={control}
+            name={SEARCH_FIELD}
+            render={({ field: { onChange, value, ref } }) => (
+              <TextInput ref={ref} value={value} onChangeText={onChange} placeholder="Search" />
+            )}
+          />
+          <TouchableIcon name={IconNameEnum.X} onPress={closeSearchField} style={styles.close} />
+        </>
+      ) : (
+        <>
+          <TouchableIcon name={IconNameEnum.Search} onPress={() => setIsShowSearchField(true)} />
+          <TouchableIcon name={IconNameEnum.Add} onPress={onPressAddIcon} />
+        </>
+      )}
+    </Row>
+  );
+};
