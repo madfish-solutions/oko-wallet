089f6dd26128832848798f11b7cfb3e7eb736317
svyatoslavtt <stiven22235@gmail.com>
2022-07-12 15:55:35 +0300

    Fix useFlatListRef warning

--- libs/ui/src/modals/hooks/use-flat-list-ref.hook.ts
+++ libs/ui/src/modals/hooks/use-flat-list-ref.hook.ts
@@ -1,22 +1,20 @@
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 import { FlatList } from 'react-native-gesture-handler';
+
+import { useDelayedEffect } from '../../hooks/use-delayed-effect.hook';
 
 export const useFlatListRef = <T>({ data, selectedIndex }: { data: T[]; selectedIndex: number }) => {
   const flatListRef = useRef<FlatList>(null);
 
-  useEffect(() => {
-    const scrollToSelectedIndex = async () => {
-      await new Promise(resolve => setTimeout(resolve, 100));
+  const scrollToSelectedIndex = () => {
+    const index = selectedIndex > -1 ? selectedIndex : 0;
 
-      const index = selectedIndex > -1 ? selectedIndex : 0;
+    if (data.length && selectedIndex > -1) {
+      flatListRef.current?.scrollToIndex({ index, animated: true });
+    }
+  };
 
-      if (data.length && selectedIndex > -1) {
-        flatListRef.current?.scrollToIndex({ index, animated: true });
-      }
-    };
-
-    scrollToSelectedIndex();
-  }, [selectedIndex]);
+  useDelayedEffect(scrollToSelectedIndex, [selectedIndex], 0);
 
   return { flatListRef };
 };
