ecccd2be7766f5a13d57ed82b19b1858b66f4da7
Mykola Medvedenko <mukojla@ukr.net>
2022-07-14 20:06:33 +0300

    fix naming

--- libs/ui/src/utils/generate-ethereum-hd-account.util.ts
+++ libs/ui/src/utils/generate-ethereum-hd-account.util.ts
@@ -1,8 +1,8 @@
 import { mnemonicToSeed } from 'bip39';
 import { utils, Wallet } from 'ethers';
 
-export const generateEvmHdAccount = async (seedPhrase: string, derivationPath: string): Promise<Wallet> => {
-  const seed = await mnemonicToSeed(seedPhrase);
+export const generateEvmHdAccount = async (mnemonic: string, derivationPath: string): Promise<Wallet> => {
+  const seed = await mnemonicToSeed(mnemonic);
   const hdNode = utils.HDNode.fromSeed(seed);
   const derivedHdNode = hdNode.derivePath(derivationPath);
 
