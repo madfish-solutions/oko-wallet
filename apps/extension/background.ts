d418e5183cf842e40b872d8096bde78cbfa149fd
razbeikov <razbeikov.oleksandr@gmail.com>
2022-07-21 17:37:35 +0300

    changes

--- apps/extension/background.ts
+++ apps/extension/background.ts
@@ -1,14 +1,10 @@
-import WalletConnect from '@walletconnect/client';
 import browser from 'webextension-polyfill';
-
-import { WalletConnectSession } from '../../libs/ui/src/interfaces/connect-wallet.interface';
 
 console.log('Background script exist...');
 
-let localStorageValue: WalletConnectSession;
+// const ethereum = window.ethereum;
 
-let connector;
-
+// ethereum.on('connect', connectInfo => console.log(connectInfo));
 // browser.action.onClicked.addListener(async tab => {
 //   try {
 //     console.log('onClicked event...');
@@ -28,25 +24,7 @@ browser.runtime.onMessage.addListener(async (msg, sender) => {
   console.log(msg, 'MESSAGE');
   const result = JSON.parse(msg.content);
   console.log(result);
-  if (result.status === 'success') {
-    const getData = localStorage.getItem('walletconnect');
-    if (getData !== null) {
-      localStorageValue = JSON.parse(getData);
-    }
 
-    connector = new WalletConnect({ session: localStorageValue });
-    console.log(connector);
-
-    connector.on('call_request', async (error, payload) => {
-      console.log('call request');
-      console.log(payload);
-      window.postMessage(payload, '*');
-      browser.runtime.sendMessage({ background: payload });
-      if (error !== null) {
-        throw `call_request: ${error}`;
-      }
-    });
-  }
   if (msg.method === 'eth_sendTransaction') {
     await browser.windows.create({
       type: 'popup',
--- apps/extension/contentScript.ts
+++ apps/extension/contentScript.ts
@@ -2,17 +2,16 @@ import browser from 'webextension-polyfill';
 
 window.addEventListener('message', async evt => {
   console.log(evt);
-  if (evt.data.method === 'eth_sendTransaction') {
-    await browser.windows.create({
-      type: 'popup',
-      url: browser.runtime.getURL('index.html'),
-      width: 380,
-      height: 630,
-      top: 20,
-      left: 20
-    });
-
-    browser.runtime.sendMessage(evt.data);
-    console.log(evt.data.method);
-  }
+  browser.runtime.sendMessage(evt.data);
+  console.log(evt.data);
+  // if (evt.data.method === 'eth_sendTransaction') {
+  //   await browser.windows.create({
+  //     type: 'popup',
+  //     url: browser.runtime.getURL('index.html'),
+  //     width: 380,
+  //     height: 630,
+  //     top: 20,
+  //     left: 20
+  //   });
+  // }
 });
