dfa3fe6d76fdfd3eeb29489ea0f73d141e56bb09
svyatoslavtt <stiven22235@gmail.com>
2022-07-25 20:35:42 +0300

    Add some fix

--- libs/ui/src/utils/unlock-app-state.ts
+++ libs/ui/src/utils/unlock-app-state.ts
@@ -6,20 +6,20 @@ import { isMobile, isWeb } from './platform.utils';
 
 const LOCK_TIME = 'locktime';
 
-export const getUnlockedAppState = (defaulValue: boolean) => {
+export const getUnlockedAppState = (defaultValue: boolean) => {
   if (isMobile) {
-    return defaulValue;
+    return defaultValue;
   }
 
-  const timelock: number | null = JSON.parse(localStorage.getItem(LOCK_TIME) as string);
+  const locktime: number | null = JSON.parse(localStorage.getItem(LOCK_TIME) as string);
 
-  if (isDefined(timelock) && timelock > 0) {
-    const timeIsLeft = Date.now() > ONE_MINUTE * MEMOIZE_UNLOCKED_STATE + +timelock;
+  if (isDefined(locktime) && locktime > 0) {
+    const timeIsLeft = Date.now() > ONE_MINUTE * MEMOIZE_UNLOCKED_STATE + locktime;
 
     return timeIsLeft;
   }
 
-  return defaulValue;
+  return defaultValue;
 };
 
 export const setLocktimeAppValue = (value = Date.now()) =>
