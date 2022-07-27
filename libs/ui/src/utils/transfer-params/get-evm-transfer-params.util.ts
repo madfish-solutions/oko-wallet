20624a3e5c59980fbdc416e60dbaa24049e81285
Mykola Medvedenko <mukojla@ukr.net>
2022-07-27 10:53:13 +0300

    fixes

--- libs/ui/src/utils/transfer-params/get-evm-transfer-params.util.ts
+++ libs/ui/src/utils/transfer-params/get-evm-transfer-params.util.ts
@@ -2,11 +2,5 @@ import { of } from 'rxjs';
 
 import { SendAssetPayload } from '../../interfaces/send-asset-action-payload.interface';
 
-export const getEvmTransferParams$ = ({ receiverPublicKeyHash, amount, asset }: SendAssetPayload) => {
-  const evmTransferParam = {
-    value: amount,
-    receiverPublicKeyHash
-  };
-
-  return of({ ...evmTransferParam, asset });
-};
+export const getEvmTransferParams$ = ({ receiverPublicKeyHash, amount, asset }: SendAssetPayload) =>
+  of({ value: amount, receiverPublicKeyHash, asset });
--- libs/ui/src/utils/transfer-params/get-tezos-transfer-params.util.ts
+++ libs/ui/src/utils/transfer-params/get-tezos-transfer-params.util.ts
@@ -1,10 +1,12 @@
 import { TransferParams } from '@taquito/taquito/dist/types/operations/types';
-import { from, of, Observable } from 'rxjs';
+import { from, Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 
+import { AssetTypeEnum } from '../../enums/asset-type.enum';
 import { AccountInterface } from '../../interfaces/account.interface';
 import { NetworkInterface } from '../../interfaces/network.interface';
 import { SendAssetPayload } from '../../interfaces/send-asset-action-payload.interface';
+import { getAssetType } from '../get-asset-type.util';
 import { getString } from '../get-string.utils';
 import { createReadOnlyTezosToolkit } from '../tezos-toolkit.utils';
 import { tezosFormatUnits } from '../units.utils';
@@ -18,17 +20,16 @@ export const getTezosTransferParams$ = (
   const { rpcUrl, networkType } = selectedNetwork;
   const senderPublicKeyHash = getString(sender.networksKeys[networkType]?.publicKeyHash);
   const amountBN = tezosFormatUnits(amount, decimals);
-  const isFA2Token = tokenId !== '';
-  const isGasToken = tokenAddress === '';
+  const assetType = getAssetType(asset);
 
-  return isGasToken
+  return assetType === AssetTypeEnum.GasToken
     ? of({
         to: receiverPublicKeyHash,
         amount: Number(amount)
       })
     : from(createReadOnlyTezosToolkit(rpcUrl, sender).contract.at(tokenAddress)).pipe(
         map(contract =>
-          isFA2Token
+          assetType === AssetTypeEnum.Collectible
             ? contract.methods.transfer([
                 {
                   from_: senderPublicKeyHash,
