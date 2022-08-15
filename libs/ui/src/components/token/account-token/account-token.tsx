6c50e85caf6d4d455813057e00f86747e638462a
svyatoslavtt <stiven22235@gmail.com>
2022-08-15 15:33:12 +0300

    Add swicther

--- libs/ui/src/components/token/account-token/account-token.tsx
+++ libs/ui/src/components/token/account-token/account-token.tsx
@@ -1,6 +1,5 @@
 import { isString } from '@rnw-community/shared';
 import React, { FC, useEffect } from 'react';
-import { View } from 'react-native';
 import { useDispatch } from 'react-redux';
 
 import { Token } from '../../../interfaces/token.interface';
@@ -8,12 +7,10 @@ import { AddHideButtonsEnum } from '../../../screens/tokens/enums';
 import { getImageSource } from '../../../screens/wallet/components/assets-widget/utils/get-image-source.util';
 import { changeTokenVisibilityAction, loadAccountTokenBalanceAction } from '../../../store/wallet/wallet.actions';
 import { formatUnits } from '../../../utils/units.utils';
-import { Button } from '../../button/button';
-import { ButtonSizeEnum } from '../../button/enums';
+import { SwitchThemesEnum } from '../../switch/enum';
+import { Switch } from '../../switch/switch';
 import { TokenItemThemesEnum } from '../token-item/enums';
 import { TokenItem } from '../token-item/token-item';
-
-import { styles } from './account-token.styles';
 
 interface Props {
   token: Token;
@@ -40,14 +37,7 @@ export const AccountToken: FC<Props> = ({ token, showButton, loadBalance = false
   return (
     <TokenItem imageSource={imageSource} balance={formattedBalance} symbol={symbol} theme={theme} name={name}>
       {isString(showButton) ? (
-        <View style={styles.buttonContainer}>
-          <Button
-            title={showButton}
-            size={ButtonSizeEnum.Small}
-            onPress={handleTokenVisibility}
-            styleText={[styles.buttonText, showButton === AddHideButtonsEnum.Hide && styles.redText]}
-          />
-        </View>
+        <Switch onPress={handleTokenVisibility} theme={SwitchThemesEnum.Primary} isActive={token.isVisible} />
       ) : undefined}
     </TokenItem>
   );
