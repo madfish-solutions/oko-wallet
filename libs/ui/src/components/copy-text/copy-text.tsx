7e5adf60aeeb6d7d16d4cd312891a1e2c99a6219
svyatoslavtt <stiven22235@gmail.com>
2022-08-29 15:08:22 +0300

    Fix review

--- libs/ui/src/components/copy-text/copy-text.tsx
+++ libs/ui/src/components/copy-text/copy-text.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import { TouchableOpacity } from 'react-native';
 
+import { TextStyleProps } from '../../interfaces/style.interface';
 import { handleCopyToClipboard } from '../../utils/copy-to-clipboard.util';
 import { shortize } from '../../utils/shortize.util';
 import { Text } from '../text/text';
@@ -8,18 +9,19 @@ import { Text } from '../text/text';
 import { styles } from './copy-text.styles';
 
 interface Props {
-  address: string;
+  text: string;
   isShortize?: boolean;
   numberOfLines?: number;
+  style?: TextStyleProps;
 }
 
-export const CopyText: FC<Props> = ({ address, isShortize = true, numberOfLines }) => {
-  const copy = () => handleCopyToClipboard(address);
+export const CopyText: FC<Props> = ({ text, isShortize = true, numberOfLines, style }) => {
+  const copy = () => handleCopyToClipboard(text);
 
   return (
     <TouchableOpacity onPress={copy}>
-      <Text style={styles.root} numberOfLines={numberOfLines}>
-        {isShortize ? shortize(address) : address}
+      <Text style={[styles.root, style]} numberOfLines={numberOfLines}>
+        {isShortize ? shortize(text) : text}
       </Text>
     </TouchableOpacity>
   );
