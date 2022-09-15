9c87386041a1c8e7c2f9444f2a0f90a50ae8ef9f
svyatoslavtt <stiven22235@gmail.com>
2022-09-15 17:26:10 +0300

    Add improves

--- libs/ui/src/screens/collectibles/components/nft-render-item/nft-render-item.tsx
+++ libs/ui/src/screens/collectibles/components/nft-render-item/nft-render-item.tsx
@@ -3,7 +3,6 @@ import { Pressable, View } from 'react-native';
 
 import { Text } from '../../../../components/text/text';
 import { Token } from '../../../../interfaces/token.interface';
-import { getTokenSlug } from '../../../../utils/token.utils';
 
 import { styles } from './nft-render-item.styles';
 
@@ -15,11 +14,7 @@ interface Props {
 }
 
 export const NftRenderItem: FC<Props> = ({ nft, name, handleItemPress, index, children }) => (
-  <Pressable
-    key={getTokenSlug(nft.tokenAddress, nft.tokenId)}
-    onPress={() => handleItemPress(nft)}
-    style={[styles.nft, index % 2 === 0 && styles.marginRight]}
-  >
+  <Pressable onPress={() => handleItemPress(nft)} style={[styles.nft, index % 2 === 0 && styles.marginRight]}>
     <View style={styles.imageWrapper}>{children}</View>
 
     <Text style={styles.nftName}>{name}</Text>
