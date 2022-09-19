2a6887fd9e368d4278b3cd3eb3292bf7de9b12b0
svyatoslavtt <stiven22235@gmail.com>
2022-09-19 13:25:35 +0300

    Remove comment

--- libs/ui/src/components/navigator/utils/maximise-view-options.ts
+++ libs/ui/src/components/navigator/utils/maximise-view-options.ts
@@ -2,7 +2,6 @@ import { ViewStyle } from 'react-native';
 
 import { getCustomSize } from '../../../styles/format-size';
 
-// maximise-view options
 const verticalOffset = getCustomSize(7.5); // top: 60px, bottom: 60px
 export const extensionHeight = window.innerHeight - verticalOffset * 2;
 export const maximiseViewDynamicStyles = { marginTop: verticalOffset, height: extensionHeight };
