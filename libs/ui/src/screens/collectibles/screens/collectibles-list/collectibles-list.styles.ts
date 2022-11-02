f37bdfcf2cfe738a4aed6a9e22c3cefd900159ba
Svyatoslav Tyutyunnik <stiven22235@gmail.com>
2022-11-02 20:35:02 +0200

    Kl 173 adapt the size of nft to different screens on the info for one nft page (#145)
    * Test commit
    
    * Fix collectibleSize offset
    
    * Fix image size
    
    * Fix layout
    
    * Check container width
    
    * Add onLayout to View
    
    * Add onLayout to View
    
    * Add improves

--- libs/ui/src/screens/collectibles/screens/collectibles-list/collectibles-list.styles.ts
+++ libs/ui/src/screens/collectibles/screens/collectibles-list/collectibles-list.styles.ts
@@ -3,6 +3,7 @@ import { StyleSheet } from 'react-native';
 import { colors } from '../../../../styles/colors';
 import { getCustomSize } from '../../../../styles/format-size';
 import { typography } from '../../../../styles/typography';
+import { COLLECTIBLE_SIZE } from '../../constants';
 
 export const styles = StyleSheet.create({
   layoutContainer: {
@@ -14,16 +15,16 @@ export const styles = StyleSheet.create({
   },
   imageContainer: {
     position: 'absolute',
-    maxHeight: getCustomSize(37.5),
-    maxWidth: getCustomSize(37.5),
+    maxHeight: COLLECTIBLE_SIZE,
+    maxWidth: COLLECTIBLE_SIZE,
     padding: '5.5%',
     width: '100%',
     height: '100%'
   },
   collectionSize: {
     position: 'absolute',
-    top: getCustomSize(1.5),
-    right: getCustomSize(1.5),
+    top: '7.5%',
+    right: '7.5%',
     alignItems: 'center',
     justifyContent: 'center',
     height: getCustomSize(2),
