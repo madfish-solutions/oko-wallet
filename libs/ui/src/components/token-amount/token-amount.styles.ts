8b6c6c1484feed089b2db309b77ef0ef2ac4bb94
svyatoslavtt <stiven22235@gmail.com>
2022-11-02 19:14:20 +0200

    Fix bugs

--- libs/ui/src/components/token-amount/token-amount.styles.ts
+++ libs/ui/src/components/token-amount/token-amount.styles.ts
@@ -4,13 +4,13 @@ import { getCustomSize } from '../../styles/format-size';
 import { typography } from '../../styles/typography';
 
 export const styles = StyleSheet.create({
-  lessSign: {
-    position: 'relative',
-    bottom: getCustomSize(0.125),
-    marginRight: getCustomSize(0.5)
-  },
   text: {
     ...typography.numbersIBMPlexSansMedium13
+  },
+  lessSign: {
+    position: 'relative',
+    bottom: getCustomSize(0.0625),
+    marginRight: getCustomSize(0.5)
   },
   symbol: {
     marginLeft: getCustomSize(0.25)
--- libs/ui/src/components/token-amount/token-amount.tsx
+++ libs/ui/src/components/token-amount/token-amount.tsx
@@ -20,7 +20,7 @@ export const TokenAmount: FC<Props> = ({ value, symbol, style }) => {
 
   return (
     <Row>
-      {isSmallValue && <Text style={[textStyle, styles.lessSign]}>{'<'}</Text>}
+      {isSmallValue && <Text style={[textStyle, styles.lessSign]}>&#60;</Text>}
       <Text style={textStyle}>{isSmallValue ? value.replace('<', '').trim() : value}</Text>
       {isNotEmptyString(symbol) && <Text style={[textStyle, styles.symbol]}>{symbol}</Text>}
     </Row>
