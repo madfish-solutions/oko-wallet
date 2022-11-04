4cac82e70877cbca005b9daee97536dcfb123b35
Svyatoslav Tyutyunnik <stiven22235@gmail.com>
2022-11-04 15:43:19 +0200

    [KL-268] Add opportunity to create an account with 15-18-21 words of mnemonic (#149)
    * Add all mnemonic length
    
    * Remove unsed code

--- libs/ui/src/constants/seed-words-amount.ts
+++ libs/ui/src/constants/seed-words-amount.ts
@@ -53,19 +53,4 @@ export const words: SeedWordsAmount[] = [
   }
 ];
 
-export const createANewWalletWords: SeedWordsAmount[] = [
-  {
-    id: 1,
-    title: 'I  have  a 12 word phrase',
-    value: MnemonicLengthEnum.Twelve,
-    testID: SeedWordsAmountTestIDs.WordsAmount12
-  },
-  {
-    id: 5,
-    title: 'I  have  a 24 word phrase',
-    value: MnemonicLengthEnum.TwentyFour,
-    testID: SeedWordsAmountTestIDs.WordsAmount24
-  }
-];
-
 export const maxWordsLength = Array(words.slice(-1)[0].value).fill('');
