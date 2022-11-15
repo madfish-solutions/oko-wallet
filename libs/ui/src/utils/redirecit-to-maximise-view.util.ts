41d4175815a9572c46b75f9221975db4b96417f3
razbeikov <razbeikov.oleksandr@gmail.com>
2022-11-15 11:30:16 +0200

    fix import conflict

--- libs/ui/src/utils/redirecit-to-maximise-view.util.ts
+++ libs/ui/src/utils/redirecit-to-maximise-view.util.ts
@@ -1,5 +1,6 @@
+import { isMaximiseScreen } from './check-active-application-session.util';
 import { openMaximiseScreen } from './open-maximise-screen.util';
-import { isMaximiseScreen, isWeb } from './platform.utils';
+import { isWeb } from './platform.utils';
 
 export const redirectToMamixiseView = () => {
   if (isWeb && !isMaximiseScreen) {
