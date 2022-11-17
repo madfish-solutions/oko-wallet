2445669c7ca77173a5c3d7b250dc2a55d96d04b4
Vlad <vladnaumenko@madfish.solutions>
2022-11-17 14:57:24 +0200

    Fix extension path on windows

--- apps/extension-e2e/src/hooks/before-all.hook.ts
+++ apps/extension-e2e/src/hooks/before-all.hook.ts
@@ -4,7 +4,9 @@ import { BrowserContext } from '../classes/browser-context.class';
 import { initBrowserContext } from '../utils/browser-context.utils';
 import { getExtensionId, initBrowser } from '../utils/browser.utils';
 
-BeforeAll(async () => {
+const LONG_TIMEOUT = 20 * 1000;
+
+BeforeAll({ timeout: LONG_TIMEOUT }, async () => {
   const browser = await initBrowser();
 
   const extensionId = await getExtensionId(browser);
