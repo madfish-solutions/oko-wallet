15500ab01ac69761ad4474d47617c80cdc85d796
Andrii Ivaniv <unixvb@gmail.com>
2022-11-20 18:31:25 +0200

    mobile fixes

--- libs/ui/src/components/navigator/utils/maximise-view-options.ts
+++ libs/ui/src/components/navigator/utils/maximise-view-options.ts
@@ -6,6 +6,7 @@ const verticalOffset = getCustomSize(7.5); // top: 60px, bottom: 60px
 export const extensionHeight = window.innerHeight - verticalOffset * 2;
 export const maximiseViewStyles = {
   marginTop: verticalOffset,
+  // TODO: Sviat - fix this (mobile splash-screen.ts do not work)
   height: extensionHeight,
   borderRadius: getCustomSize(3),
   overflow: 'hidden' as ViewStyle['overflow']
---
+++ libs/ui/src/modals/screens/network-change-confirmation/network-change-confirmation.native.tsx
@@ -0,0 +1,3 @@
+import React, { FC } from 'react';
+
+export const NetworkChangeConfirmation: FC = () => null;
