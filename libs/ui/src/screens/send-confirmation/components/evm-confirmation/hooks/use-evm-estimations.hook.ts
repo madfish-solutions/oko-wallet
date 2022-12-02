ffd410f8c816679628db9af5066047c9b371e6e7
Mykola Medvedenko <mukojla@ukr.net>
2022-12-02 15:56:30 +0200

    refactor gasLimit

--- libs/ui/src/screens/send-confirmation/components/evm-confirmation/hooks/use-evm-estimations.hook.ts
+++ libs/ui/src/screens/send-confirmation/components/evm-confirmation/hooks/use-evm-estimations.hook.ts
@@ -1,12 +1,10 @@
 import { useEffect, useMemo, useState } from 'react';
 
-import { Erc20Abi__factory, Erc721Abi__factory, Erc1155Abi__factory } from '../../../../../contract-types';
 import { AssetTypeEnum } from '../../../../../enums/asset-type.enum';
 import { Asset } from '../../../../../interfaces/asset.interface';
 import { NetworkInterface } from '../../../../../interfaces/network.interface';
-import { checkIsErc721Collectible } from '../../../../../utils/check-is-erc721-collectible.util';
 import { getDefaultEvmProvider } from '../../../../../utils/get-default-evm-provider.utils';
-import { getAmount } from '../utils/get-amount.util';
+import { getGasLimit } from '../utils/get-gas-limit.util';
 import { modifyGasLimit } from '../utils/modify-gas-limit.util';
 
 interface UseEvmEstimationsArgs {
@@ -20,7 +18,7 @@ interface UseEvmEstimationsArgs {
 
 export const useEvmEstimations = ({
   network: { rpcUrl },
-  asset: { tokenAddress, decimals, tokenId, standard },
+  asset,
   receiverPublicKeyHash,
   value,
   publicKeyHash,
@@ -34,43 +32,7 @@ export const useEvmEstimations = ({
 
     const getEstimations = async () => {
       const provider = getDefaultEvmProvider(rpcUrl);
-
-      let gasLimit;
-
-      switch (assetType) {
-        case AssetTypeEnum.GasToken:
-          gasLimit = await provider
-            .estimateGas({ to: receiverPublicKeyHash, value: getAmount(value, decimals) })
-            .catch(console.log);
-          break;
-
-        case AssetTypeEnum.Collectible:
-          const isErc721 = checkIsErc721Collectible({ standard });
-
-          if (isErc721) {
-            const contract721 = Erc721Abi__factory.connect(tokenAddress, provider);
-
-            gasLimit = await contract721.estimateGas
-              .transferFrom(publicKeyHash, receiverPublicKeyHash, tokenId)
-              .catch(console.log);
-            break;
-          } else {
-            const contract1155 = Erc1155Abi__factory.connect(tokenAddress, provider);
-
-            gasLimit = await contract1155.estimateGas
-              .safeTransferFrom(publicKeyHash, receiverPublicKeyHash, tokenId, value, [])
-              .catch(console.log);
-            break;
-          }
-
-        default:
-          const contract20 = Erc20Abi__factory.connect(tokenAddress, provider);
-
-          gasLimit = await contract20.estimateGas
-            .transfer(receiverPublicKeyHash, getAmount(value, decimals))
-            .catch(console.log);
-      }
-
+      const gasLimit = await getGasLimit(asset, assetType, receiverPublicKeyHash, publicKeyHash, value, rpcUrl);
       const fee = await provider.getFeeData();
 
       setEstimations({ gasLimit: modifyGasLimit(gasLimit), gasPrice: fee.gasPrice?.toNumber() ?? 0 });
@@ -78,7 +40,7 @@ export const useEvmEstimations = ({
     };
 
     getEstimations();
-  }, [rpcUrl, tokenAddress]);
+  }, [rpcUrl, asset.tokenAddress]);
 
   return useMemo(() => ({ estimations, isLoading }), [estimations, isLoading]);
 };
