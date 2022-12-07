2cd30736a48bb88c386d0718e8ef869123cf57ed
Mykola Medvedenko <mukojla@ukr.net>
2022-12-07 15:59:24 +0200

    add zindex, because toast was visible

--- libs/ui/src/screens/splash-screen/splash-screen.styles.ts
+++ libs/ui/src/screens/splash-screen/splash-screen.styles.ts
@@ -14,7 +14,8 @@ export const styles = StyleSheet.create({
       left: 0,
       top: 0,
       width: '100%',
-      height: '100%'
+      height: '100%',
+      zIndex: 100000000
     }),
     ...(isWeb && maximiseViewStyles)
   }
