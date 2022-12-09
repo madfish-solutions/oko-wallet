4916d94e4e347271b28b7db067e237ce94dc5d2f
Mykola Medvedenko <35599200+MuKoJla@users.noreply.github.com>
2022-12-09 14:07:50 +0200

    [KL-194] Add secure screen (#185)
    * Add secure screen
    
    * secure screen for all pages
    
    * add zindex, because toast was visible
    
    * refactor useShowSecurityScreen hook
    
    * add android and ios files to ignore

--- libs/ui/src/screens/splash-screen/splash-screen.styles.ts
+++ libs/ui/src/screens/splash-screen/splash-screen.styles.ts
@@ -2,12 +2,21 @@ import { StyleSheet } from 'react-native';
 
 import { maximiseViewStyles } from '../../components/navigator/utils/maximise-view-options';
 import { colors } from '../../styles/colors';
+import { isMobile, isWeb } from '../../utils/platform.utils';
 
 export const styles = StyleSheet.create({
   root: {
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: colors.bgGrey1,
-    ...maximiseViewStyles
+    ...(isMobile && {
+      position: 'absolute',
+      left: 0,
+      top: 0,
+      width: '100%',
+      height: '100%',
+      zIndex: 100000000
+    }),
+    ...(isWeb && maximiseViewStyles)
   }
 });
