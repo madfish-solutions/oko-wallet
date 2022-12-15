9d8710dfa80fd6d0f3c824ca8aa0a98fac13602c
Andrii Ivaniv <unixvb@gmail.com>
2022-12-15 13:53:36 +0200

    postinstall.sh removed

--- apps/mobile/postinstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-deprecated_types_replace_string="} from 'react-native'\nimport { ViewPropTypes } from 'deprecated-react-native-prop-types'"
-
-sed -i "" "s/  ViewPropTypes,//" node_modules/react-native-camera/src/RNCamera.js
-sed -i "" "s/} from 'react-native';/$deprecated_types_replace_string/" node_modules/react-native-camera/src/RNCamera.js
