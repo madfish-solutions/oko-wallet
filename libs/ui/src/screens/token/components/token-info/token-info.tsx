8f2940efc9494bc42f1b1fd56962769cb360eefd
svyatoslavtt <stiven22235@gmail.com>
2022-12-21 14:32:40 +0200

    Remove web field

--- libs/ui/src/screens/token/components/token-info/token-info.tsx
+++ libs/ui/src/screens/token/components/token-info/token-info.tsx
@@ -1,5 +1,4 @@
 import { RouteProp, useRoute } from '@react-navigation/native';
-import { isString } from 'lodash';
 import React, { FC } from 'react';
 import { ScrollView } from 'react-native';
 
@@ -10,8 +9,6 @@ import { ScreensEnum, ScreensParamList } from '../../../../enums/sreens.enum';
 import { useSelectedNetworkSelector } from '../../../../store/wallet/wallet.selectors';
 import { getString } from '../../../../utils/get-string.utils';
 import { getTokenDetailsUrl } from '../../../../utils/get-token-details-url.util';
-import { removeTrailingSlash } from '../../../../utils/remove-trailing-slash.util';
-import { eraseProtocol } from '../../../../utils/string.util';
 
 import { styles } from './token-info.styles';
 
@@ -23,9 +20,6 @@ export const TokenInfo: FC = () => {
   } = useRoute<RouteProp<ScreensParamList, ScreensEnum.Token>>();
   const { explorerUrl, networkType } = useSelectedNetworkSelector();
 
-  // TODO: get data from api
-  const web = undefined;
-
   const tokenMetadata = {
     ...(tokenAddress !== GAS_TOKEN_ADDRESS && {
       contract: {
@@ -34,11 +28,6 @@ export const TokenInfo: FC = () => {
         prompt: getTokenDetailsUrl(tokenAddress, getString(explorerUrl), networkType)
       }
     }),
-    web: {
-      name: 'Web',
-      value: isString(web) ? eraseProtocol(removeTrailingSlash(web)) : web,
-      prompt: web
-    },
     decimal: {
       name: 'Decimal',
       value: decimals.toString(),
