2208f05caa8932e4170978db1f2e599612a07378
Andrii Ivaniv <unixvb@gmail.com>
2022-12-22 16:51:49 +0200

    mobile dotenv setup

---
+++ libs/ui/src/utils/env.utils.native.ts
@@ -0,0 +1,5 @@
+// eslint-disable-next-line import/no-named-as-default
+import Config from 'react-native-config';
+
+/* eslint-disable dot-notation */
+export const BACKEND_URL = Config['BACKEND_URL'];
