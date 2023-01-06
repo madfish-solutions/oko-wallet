c61fb03d1106a2e1083261bd11d9b5eb26244262
razbeikov <razbeikov.oleksandr@gmail.com>
2023-01-06 10:03:02 +0200

    fix error when service worker died

--- apps/extension/content-script.ts
+++ apps/extension/content-script.ts
@@ -6,7 +6,44 @@ import { runtime } from 'webextension-polyfill';
 import { DAppMessage } from './src/interfaces/dapp-message.interface';
 import { getWindowMetadata } from './src/utils/window.utils';
 
-const myPort = runtime.connect({ name: 'port-from-cs' });
+function connectPort() {
+  const myPort = runtime.connect({ name: 'port-from-cs' });
+
+  // listen background-script message and send message to dApps
+  myPort.onMessage.addListener(async message => {
+    window.postMessage(message, '*');
+
+    return Promise.resolve();
+  });
+
+  // listen dApps and send message to background-script
+  window.addEventListener('message', async message => {
+    if (message.data?.target === 'oko-contentscript') {
+      const windowMetadata = await getWindowMetadata();
+
+      const dAppMessage: DAppMessage = {
+        data: message.data,
+        sender: {
+          origin: message.origin,
+          name: windowMetadata.name,
+          favicon: windowMetadata.favicon
+        }
+      };
+
+      myPort.postMessage(dAppMessage);
+
+      return Promise.resolve();
+    }
+  });
+
+  myPort.onDisconnect.addListener(() => {
+    connectPort();
+  });
+
+  return myPort;
+}
+
+connectPort();
 
 const CONTENT_SCRIPT = 'oko-contentscript';
 const INPAGE = 'oko-inpage';
@@ -15,33 +52,6 @@ const PROVIDER = 'oko-provider';
 const pageStream = new WindowPostMessageStream({
   name: CONTENT_SCRIPT,
   target: INPAGE
-});
-
-// listen background-script message and send message to dApps
-myPort.onMessage.addListener(async message => {
-  window.postMessage(message, '*');
-
-  return Promise.resolve();
-});
-
-// listen dApps and send message to background-script
-window.addEventListener('message', async message => {
-  if (message.data?.target === 'oko-contentscript') {
-    const windowMetadata = await getWindowMetadata();
-
-    const dAppMessage: DAppMessage = {
-      data: message.data,
-      sender: {
-        origin: message.origin,
-        name: windowMetadata.name,
-        favicon: windowMetadata.favicon
-      }
-    };
-
-    myPort.postMessage(dAppMessage);
-
-    return Promise.resolve();
-  }
 });
 
 // listen UI and send message to dApps
