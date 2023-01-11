df14fe27ad53ded6cf156c70c7bfc8eae4db0b2b
svyatoslavtt <stiven22235@gmail.com>
2023-01-11 13:14:06 +0200

    Fix tokenAddress for request activity

--- libs/ui/src/screens/token/components/activity/activity.tsx
+++ libs/ui/src/screens/token/components/activity/activity.tsx
@@ -1,7 +1,9 @@
 import { RouteProp, useRoute } from '@react-navigation/native';
 import React, { FC } from 'react';
 
+import { getDebankId } from '../../../../api/utils/get-debank-id.util';
 import { ActivitySectionList } from '../../../../components/activity-section-list/activity-section-list';
+import { GAS_TOKEN_ADDRESS } from '../../../../constants/defaults';
 import { ScreensEnum, ScreensParamList } from '../../../../enums/sreens.enum';
 import {
   useSelectedAccountPublicKeyHashSelector,
@@ -11,11 +13,16 @@ import {
 export const Activity: FC = () => {
   const {
     params: {
-      token: { tokenAddress }
+      token: { tokenAddress: basicTokenAddress }
     }
   } = useRoute<RouteProp<ScreensParamList, ScreensEnum.Token>>();
   const publicKeyHash = useSelectedAccountPublicKeyHashSelector();
   const { chainId } = useSelectedNetworkSelector();
+
+  const tokenAddress =
+    getDebankId(chainId) !== 'klay' && basicTokenAddress === GAS_TOKEN_ADDRESS
+      ? getDebankId(chainId)
+      : basicTokenAddress;
 
   return <ActivitySectionList publicKeyHash={publicKeyHash} chainId={chainId} tokenAddress={tokenAddress} />;
 };
