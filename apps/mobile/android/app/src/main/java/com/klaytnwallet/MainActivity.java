a5fe90af4e31613644ae63edd4b96468b58ca850
Svyatoslav Tyutyunnik <stiven22235@gmail.com>
2023-01-12 10:45:11 +0200

    [KL-397] - [Mobile] Add app logo and change app name on mobile (#243)
    * Add new icon and app name
    
    * Fix review
    
    * Fix review
    
    * Fix review

--- apps/mobile/android/app/src/main/java/com/klaytnwallet/MainActivity.java
+++ apps/mobile/android/app/src/main/java/com/klaytnwallet/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "KlaytnWallet";
+    return "Oko Wallet";
   }
 
   /**
