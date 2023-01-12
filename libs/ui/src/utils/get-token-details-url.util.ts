645e2405aab17e9137dfd8463c31f3268813da00
svyatoslavtt <stiven22235@gmail.com>
2023-01-12 17:00:18 +0200

    Fix explorer link

--- libs/ui/src/utils/get-token-details-url.util.ts
+++ libs/ui/src/utils/get-token-details-url.util.ts
@@ -1,7 +1,27 @@
+import { isDefined } from '@rnw-community/shared';
+
+import { getDebankId } from '../api/utils/get-debank-id.util';
 import { NetworkTypeEnum } from '../enums/network-type.enum';
 
-export const getTokenDetailsUrl = (value: string, explorerUrl: string, networkType = NetworkTypeEnum.EVM) => {
-  const explorerUrlPrefix = networkType === NetworkTypeEnum.EVM ? '/token/' : '';
+interface TokenDetails {
+  address: string;
+  id?: string;
+  explorerUrl: string;
+  networkType: NetworkTypeEnum;
+  chainId?: string;
+}
 
-  return `${explorerUrl}${explorerUrlPrefix}${value}`;
+export const getTokenDetailsUrl = ({
+  address,
+  id,
+  explorerUrl,
+  networkType = NetworkTypeEnum.EVM,
+  chainId
+}: TokenDetails) => {
+  const isKlaytn = isDefined(chainId) && getDebankId(chainId) === 'klay';
+  const searchParam = isDefined(id) && isKlaytn ? '/nft/' : '/token/';
+  const explorerUrlPrefix = networkType === NetworkTypeEnum.EVM ? searchParam : '';
+  const collectibleParam = isDefined(chainId) && isKlaytn ? '/' + id : `?a=${id}`;
+
+  return `${explorerUrl}${explorerUrlPrefix}${address}${isDefined(id) ? collectibleParam : ''}`;
 };
