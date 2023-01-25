21c99f4ce062d0c7c4efa98c0adc9d80156a7652
Mykola Medvedenko <35599200+MuKoJla@users.noreply.github.com>
2023-01-25 14:37:08 +0200

    Add edit button for token page (#261)

--- libs/ui/src/screens/token/token.styles.ts
+++ libs/ui/src/screens/token/token.styles.ts
@@ -2,6 +2,7 @@ import { StyleSheet } from 'react-native';
 
 import { colors } from '../../styles/colors';
 import { getCustomSize } from '../../styles/format-size';
+import { typography } from '../../styles/typography';
 
 export const styles = StyleSheet.create({
   root: {
@@ -11,5 +12,21 @@ export const styles = StyleSheet.create({
     width: '100%',
     height: getCustomSize(0.5),
     backgroundColor: colors.bgGrey1
+  },
+  editTokenButton: {
+    marginBottom: getCustomSize()
+  },
+  editText: {
+    ...typography.taglineInterSemiBoldUppercase13,
+    marginRight: getCustomSize(0.5)
+  },
+  disabled: {
+    color: colors.bgGrey5
+  },
+  editIcon: {
+    marginRight: getCustomSize(2)
+  },
+  tabs: {
+    justifyContent: 'space-between'
   }
 });
