fab8f485205a5ed2cd71ceaf96b3e297a0712e77
Illia Likhoshva <ilyaliko64@gmail.com>
2023-01-26 11:06:15 +0200

    [KL-500] Move backend into monorepo (#263)
    * Init inject the backend
    
    * Format & remove unused
    
    * add optional env
    
    * remove unused «lints», fix links and aliases
    
    * modify package.json scripts
    
    * Edit eslint
    
    * Update dependencies, prepare backend-types lib
    
    * add semi

---
+++ apps/backend/src/apps/debank/header.ts
@@ -0,0 +1,13 @@
+import { IncomingHttpHeaders } from 'http';
+
+import { DeBankConfiguration } from '../../config';
+
+export function formDeBankHeaderWithKey(config: DeBankConfiguration): IncomingHttpHeaders {
+  const headers = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    AccessKey: config.ACCESS_KEY
+  };
+
+  return headers;
+}
---
+++ apps/backend/src/config/debank/types.ts
@@ -0,0 +1,4 @@
+export type DeBankConfiguration = {
+  API_URL: string;
+  ACCESS_KEY: string;
+};
