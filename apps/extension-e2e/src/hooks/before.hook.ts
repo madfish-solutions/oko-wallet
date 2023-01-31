00465b85a34b5739eb7f375d2a7695f2ec3b80c0
razbeikov <razbeikov.oleksandr@gmail.com>
2023-01-31 17:32:36 +0200

    add error handling to before e2e hook

--- apps/extension-e2e/src/hooks/before.hook.ts
+++ apps/extension-e2e/src/hooks/before.hook.ts
@@ -7,8 +7,11 @@ import { BrowserContext, DEFAULT_PASSWORD, DEFAULT_SEED_PHRASE } from '../classe
 const clearStorageMessage: BackgroundMessage = { type: E2eMessageType.ClearStorage };
 
 Before(async () => {
-  // @ts-ignore
-  await BrowserContext.page.evaluate(message => chrome.runtime.sendMessage(undefined, message), clearStorageMessage);
+  await BrowserContext.page
+    // @ts-ignore
+    .evaluate(message => chrome.runtime.sendMessage(undefined, message), clearStorageMessage)
+    .catch(error => console.log(error));
+
   BrowserContext.seedPhrase = DEFAULT_SEED_PHRASE;
   BrowserContext.password = DEFAULT_PASSWORD;
   await BrowserContext.page.reload();
