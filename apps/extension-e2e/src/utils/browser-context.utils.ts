9d6d8ce1d65f4d49ac9a933800f8ffa0262c37ea
Svyatoslav Tyutyunnik <stiven22235@gmail.com>
2023-02-01 17:06:09 +0200

    [KL-458] Open extension full-page after installation (#274)
    * Open extension full-page after installation
    
    * Refactor e2e settings
    
    * Add sleep instead of retry
    
    * Remove async-retry
    
    * Remove unused code
    
    * Change 500ms to 1000ms sleep
    
    * Change 1000ms to 750ms sleep

--- apps/extension-e2e/src/utils/browser-context.utils.ts
+++ apps/extension-e2e/src/utils/browser-context.utils.ts
@@ -2,8 +2,21 @@ import { Browser } from 'puppeteer';
 
 import { BrowserContext } from '../classes/browser-context.class';
 
+import { getExtensionId } from './browser.utils';
+
 export const initBrowserContext = async (browser: Browser) => {
-  const [page] = await browser.pages();
+  const extensionId = await getExtensionId(browser);
+  const extensionUrl = `chrome-extension://${extensionId}/fullpage.html`;
+
+  const pages = await browser.pages();
+
+  const page = pages.find(p => p.url().startsWith(extensionUrl));
+
+  if (page == null) {
+    throw new Error('Initial extension page not found');
+  }
+
+  await pages[0].close();
 
   BrowserContext.browser = browser;
   BrowserContext.page = page;
