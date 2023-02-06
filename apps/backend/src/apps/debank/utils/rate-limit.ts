79422110e4b1d30f7858db253df6d121d59ef58f
Illia Likhoshva <ilyaliko64@gmail.com>
2023-02-06 10:54:40 +0200

    Refactor rate limit part

--- apps/backend/src/apps/debank/utils/rate-limit.ts
+++ apps/backend/src/apps/debank/utils/rate-limit.ts
@@ -1,19 +1,26 @@
-import { createNewRedisRateLimitter } from '../../utils/rate-limit';
+import { NextFunction, Request, Response } from 'express';
+import { RateLimiterRedis, RateLimiterRes } from 'rate-limiter-flexible';
+import { createClient } from 'redis';
 
-const rateLimiter = createNewRedisRateLimitter('deBank', {
+import { getRedisConfig } from '../../../config/redis';
+
+const rateLimiter = new RateLimiterRedis({
+  storeClient: createClient(getRedisConfig()),
+  keyPrefix: 'deBank-rate-limit',
   points: 20, // 20 requests
   duration: 3 // per 3 seconds by IP
 });
 
-export const rateLimiterMiddleware = (req, res, next) => {
-  rateLimiter
-    .consume(req.ip)
-    .then(consumed => {
-      console.debug(
-        `[${rateLimiter.keyPrefix}] ${req.ip} cons pts:${consumed.consumedPoints}, left pts: ${
-          consumed.remainingPoints
-        } reset in ${consumed.msBeforeNext / 1000}s`
-      );
+const formStat = (key: string, lrRes: RateLimiterRes): string =>
+  `${key} c.:${lrRes.consumedPoints} pts, l.: ${lrRes.remainingPoints} pts. Reset in ${lrRes.msBeforeNext / 1000}s`;
+
+export const rateLimiterMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  const key = req.ip;
+
+  return rateLimiter
+    .consume(key)
+    .then(rlRes => {
+      console.debug('[deBank Rate Limiter]', formStat(key, rlRes));
 
       return next();
     })
