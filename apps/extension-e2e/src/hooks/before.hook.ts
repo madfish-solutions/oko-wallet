aa677f2118c7a29a44e30410a9a027e7e36032e8
Yarmosh01 <andrewyarm29@gmail.com>
2023-02-08 19:13:16 +0200

    experimental

--- apps/extension-e2e/src/hooks/before.hook.ts
+++ apps/extension-e2e/src/hooks/before.hook.ts
@@ -3,7 +3,6 @@ import { Before } from '@cucumber/cucumber';
 import { E2eMessageType } from '../../../../libs/ui/src/messagers/enums/e2e-message-type.enum';
 import { BackgroundMessage } from '../../../../libs/ui/src/messagers/types/background-message.type';
 import { BrowserContext, DEFAULT_PASSWORD, DEFAULT_SEED_PHRASE } from '../classes/browser-context.class';
-import { sleep } from '../utils/promise.utils';
 
 const clearStorageMessage: BackgroundMessage = { type: E2eMessageType.ClearStorage };
 
@@ -15,5 +14,4 @@ Before(async () => {
   BrowserContext.seedPhrase = DEFAULT_SEED_PHRASE;
   BrowserContext.password = DEFAULT_PASSWORD;
   await BrowserContext.page.reload();
-  await sleep(1500);
 });
