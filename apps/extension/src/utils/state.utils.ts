7690b0fcf2cac7e771e84c4db1476c4159d44ae6
Andrii Ivaniv <unixvb@gmail.com>
2023-02-08 19:27:59 +0200

    initial redux state is undefined error fix

--- apps/extension/src/utils/state.utils.ts
+++ apps/extension/src/utils/state.utils.ts
@@ -1,10 +1,16 @@
-import { RootState, LocalStorage } from 'ui/background-script';
+import { isDefined } from '@rnw-community/shared';
+import { RootState, LocalStorage, initialRootState } from 'ui/background-script';
 
 export const getState = async () => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const state: Record<string, any> = {};
 
-  const serializedState: string = await LocalStorage.getItem('persist:root');
+  const serializedState: string | undefined = await LocalStorage.getItem('persist:root');
+
+  if (!isDefined(serializedState)) {
+    return initialRootState;
+  }
+
   const rawState: Record<string, string> = JSON.parse(serializedState);
 
   Object.keys(rawState).forEach(key => {
--- libs/ui/src/store/store.ts
+++ libs/ui/src/store/store.ts
@@ -1,24 +1,32 @@
 import { dAppsReducers } from './d-apps/d-apps.reducers';
-import { DAppsRootState } from './d-apps/d-apps.state';
+import { dAppsInitialState, DAppsRootState } from './d-apps/d-apps.state';
 import { rootStateEpics } from './root-state.epics';
 import { settingsReducers } from './settings/settings.reducers';
-import { SettingsRootState } from './settings/settings.state';
+import { settingsInitialState, SettingsRootState } from './settings/settings.state';
 import { swapEpics } from './swap/swap.epics';
 import { swapReducers } from './swap/swap.reducers';
-import { SwapRootState } from './swap/swap.state';
+import { swapInitialState, SwapRootState } from './swap/swap.state';
 import { tokenMarketInfoEpics } from './tokens-market-info/token-market-info.epics';
 import { tokensMarketInfoReducers } from './tokens-market-info/tokens-market-info.reducers';
-import { TokensMarketInfoRootState } from './tokens-market-info/tokens-market-info.state';
+import { tokensMarketInfoInitialState, TokensMarketInfoRootState } from './tokens-market-info/tokens-market-info.state';
 import { createStore } from './utils/create-store';
 import { walletEpics } from './wallet/wallet.epics';
 import { walletReducers } from './wallet/wallet.reducers';
-import { WalletRootState } from './wallet/wallet.state';
+import { walletInitialState, WalletRootState } from './wallet/wallet.state';
 
 export type RootState = WalletRootState &
   DAppsRootState &
   TokensMarketInfoRootState &
   SettingsRootState &
   SwapRootState;
+
+export const initialRootState: RootState = {
+  wallet: walletInitialState,
+  dApps: dAppsInitialState,
+  tokensMarketInfo: tokensMarketInfoInitialState,
+  settings: settingsInitialState,
+  swap: swapInitialState
+};
 
 export const { store, persistor } = createStore<RootState>({
   reducers: {
