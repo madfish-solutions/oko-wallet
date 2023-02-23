fb1381824f3539aa029e7ababdf9fad5676382e9
Mykola Medvedenko <35599200+MuKoJla@users.noreply.github.com>
2023-02-23 17:18:13 +0200

    Fix showing qr-code (#298)

--- libs/ui/src/screens/wallet/constants/dimensions.ts
+++ libs/ui/src/screens/wallet/constants/dimensions.ts
@@ -6,8 +6,8 @@ export const SHOW_QR_CODE = 0;
 export const HIDE_QR_CODE = getCustomSize(20);
 export const MIDDLE_VALUE = getCustomSize(9);
 
-const headerHeight = isWeb ? getCustomSize(15.25) : getCustomSize(10.75);
-const navBarHeight = getCustomSize(11.5);
+const headerHeight = isWeb ? getCustomSize(14.25) : getCustomSize(10.75);
+const navBarHeight = getCustomSize(9);
 const extraElements = headerHeight + navBarHeight;
 const qrCodeHeight = getCustomSize(20);
 
