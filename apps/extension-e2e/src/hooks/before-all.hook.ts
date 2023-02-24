a0a49dd8d2a30c4c3ec5bcbad8f4b0c8b90eeece
Yarmosh01 <andrewyarm29@gmail.com>
2023-02-24 23:44:30 +0200

    mda, namings

--- apps/extension-e2e/src/hooks/before-all.hook.ts
+++ apps/extension-e2e/src/hooks/before-all.hook.ts
@@ -4,7 +4,7 @@ import { BrowserContext } from '../classes/browser-context.class';
 import { initBrowserContext } from '../utils/browser-context.utils';
 import { initBrowser } from '../utils/browser.utils';
 
-const LONG_TIMEOUT = 1000;
+const LONG_TIMEOUT = 10000;
 const VIDEO_RECORDER_SAVE_PATH = 'video-reports/test-run.mp4';
 
 BeforeAll({ timeout: LONG_TIMEOUT }, async () => {
