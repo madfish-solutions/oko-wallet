73f08d9ad6907d6efccd79beb7ac8febed0b29a3
Andrii Ivaniv <unixvb@gmail.com>
2023-02-24 14:16:24 +0200

    e2e video uploader setup

--- apps/extension-e2e/src/hooks/before-all.hook.ts
+++ apps/extension-e2e/src/hooks/before-all.hook.ts
@@ -5,11 +5,11 @@ import { initBrowserContext } from '../utils/browser-context.utils';
 import { initBrowser } from '../utils/browser.utils';
 
 const LONG_TIMEOUT = 10000;
-const SAVE_PATH = 'video-reports/demo.mp4';
+const VIDEO_RECORDER_SAVE_PATH = 'video-reports/test-run.mp4';
 
 BeforeAll({ timeout: LONG_TIMEOUT }, async () => {
   const browser = await initBrowser();
 
   await initBrowserContext(browser);
-  await BrowserContext.recorder.start(SAVE_PATH);
+  await BrowserContext.recorder.start(VIDEO_RECORDER_SAVE_PATH);
 });
