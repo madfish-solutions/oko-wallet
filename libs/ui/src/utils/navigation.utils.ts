c20faebc23bb60cc10d5f2db9dc9c9c4c8bae537
svyatoslavtt <stiven22235@gmail.com>
2023-03-06 12:52:17 +0200

    Add ts-ignore

--- libs/ui/src/utils/navigation.utils.ts
+++ libs/ui/src/utils/navigation.utils.ts
@@ -4,8 +4,10 @@ import { ScreensEnum, ScreensParamList } from '../enums/sreens.enum';
 
 export const createNavigationRoute = <T extends ScreensEnum>(
   name: T,
+  // @ts-ignore
   params: ScreensParamList[T]
 ): Omit<Route<T>, 'key'> => ({
   name,
+  // @ts-ignore
   params
 });
